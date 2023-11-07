1ce3ce0fd60aa518404182a2b56d17b32e65a01c
Mike Williams <mdub@dogbiscuit.org>
2023-11-07 16:58:14 +1100

    Starting point: a mainline that lists S3 buckets.

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use aws_sdk_s3::{Client, Error};
+
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let shared_config = aws_config::load_from_env().await;
+    let client = Client::new(&shared_config);
+    let req = client.list_buckets();
+    let resp = req.send().await?;
+    let buckets = resp.buckets.unwrap_or_default();
+    for b in buckets.iter() {
+        let name: String = b.name.clone().unwrap().to_string();
+        println!("{}", name);
+    }
+    Ok(())
+}
